1d4a517109c30ac66bd09ca5f3988c91284f5c1a
SG <who.just.the.doctor@gmail.com>
2025-10-28 16:15:36 +0000

    Assets: windows fix

--- assets/python/build.cmd
+++ assets/python/build.cmd
@@ -5,27 +5,27 @@ pushd "%~dp0"
 REM create virtual environment if missing
 if not exist ".venv\Scripts\activate.bat" (
     echo Creating virtual environment...
-    python -m venv .venv
+    py -m venv .venv
 )
 
 REM activate the venv
 call ".venv\Scripts\activate.bat"
 
 REM install pypng if not present
-python -c "import png" 2>NUL
+py -c "import png" 2>NUL
 if errorlevel 1 (
     echo Installing pypng...
     ".venv\Scripts\pip.exe" install pypng
 )
 
 REM install lz4 if not present
-python -c "import lz4" 2>NUL
+py -c "import lz4" 2>NUL
 if errorlevel 1 (
     echo Installing lz4...
     ".venv\Scripts\pip.exe" install lz4
 )
 
 REM run the script with forwarded arguments
-python ..\..\lib\lvgl\lvgl\scripts\LVGLImage.py %*
+py ..\..\lib\lvgl\lvgl\scripts\LVGLImage.py %*
 
 popd
