525412ff789e6d74a0cedca26f38e2ab6b30290c
SkorP <skorpionm@yandex.ru>
2025-11-23 13:43:00 +0400

    reg update

--- lib/drivers/tps62868x/tps62868x_reg.h
+++ lib/drivers/tps62868x/tps62868x_reg.h
@@ -19,7 +19,6 @@
  */
 
 typedef struct {
-
     uint8_t V_RAMP_SPEED : 2; // 00 - 20mV/µs (0.25 µs/step),    01 - 10 mV/µs (0.5 µs/step),    10 - 5 mV/µs (1 µs/step),    11 - 1 mV/µs (5 µs/step, default).
     uint8_t EN_HICCUP : 1; // 0 - Disable HICCUP. Enable latching protection,    1 - Enable HICCUP, Disable latching protection.
     uint8_t EN_OUT_DISCHARGE : 1; // 0 - Disable output discharge,    1 - Enable output discharge
@@ -27,16 +26,13 @@ typedef struct {
     uint8_t SOFT_EN_DEVICE : 1; // 0 - Disable the device. All registers values are still kept,    1 - Re-enable the device with a new start-up without the tDelay period.
     uint8_t EN_FPWM_MODE_OUT_V_CHANGE : 1; // 0 - Keep the current mode status during output voltage change,    1 - Force the device in FPWM during output voltage change.
     uint8_t RESET : 1; // 1 - Reset all registers to default
-
 } TPS62868XControl;
 
 typedef struct {
-
     uint8_t UVLO : 1; // 1: The input voltage is less than UVLO threshold (falling edge).
     uint8_t RESERVED_1 : 1;
     uint8_t RESERVED_2 : 1;
     uint8_t HICCUP : 1; // 1: Device has HICCUP status once.
     uint8_t THERMINAL_WARNING : 1; // 1: Junction temperature is higher than 130°C
     uint8_t RESERVED_3 : 3;
-
 } TPS62868XStatus;
