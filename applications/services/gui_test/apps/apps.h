d4f924fa965a94981da334031cd904196839e325
SG <who.just.the.doctor@gmail.com>
2026-02-03 20:31:27 +0000

    Apps: remove gui test

--- applications/services/gui_test/apps/apps.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../app_common.h"
-
-void apps_call_render(App* app);
-
-bool apps_call_input(App* app, const GuiTestMessage* message);
-
-void apps_call_scroll(App* app);
--- applications/services/gui_test/font/fonts.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-extern const uint8_t u8g2_font_helvB08_tr[];
-extern const uint8_t u8g2_font_haxrcorp4089_tr[];
-extern const uint8_t u8g2_font_profont11_mr[];
-extern const uint8_t u8g2_font_profont22_tn[];
