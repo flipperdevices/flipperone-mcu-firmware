ec1bb2d18378491621c10b8e48da7b4c7ac8e5dd
SG <who.just.the.doctor@gmail.com>
2026-02-09 23:26:00 +0000

    ViewPort: view model

--- applications/services/gui/view_port.c
+++ applications/services/gui/view_port.c
@@ -5,12 +5,10 @@
 
 typedef struct {
     ViewPortLayoutCallback callback;
-    void* context;
 } LayoutCallback;
 
 typedef struct {
     ViewPortLayoutCallback callback;
-    void* context;
 } PostLayoutCallback;
 
 typedef struct {
@@ -23,6 +21,11 @@ typedef struct {
     void* context;
 } InputTouchCallback;
 
+typedef struct {
+    FuriMutex* mutex;
+    uint8_t data[];
+} ViewPortModelLocking;
+
 struct ViewPort {
     Gui* gui;
     FuriMutex* mutex;
@@ -33,12 +36,19 @@ struct ViewPort {
     PostLayoutCallback post_layout;
     InputCallback input;
     InputTouchCallback input_touch;
+
+    ViewPortModelType model_type;
+    void* model;
+    void* context;
 };
 
 ViewPort* view_port_alloc(void) {
     ViewPort* view_port = malloc(sizeof(ViewPort));
     view_port->is_enabled = true;
     view_port->mutex = furi_mutex_alloc(FuriMutexTypeRecursive);
+    view_port->model_type = ViewPortModelTypeNone;
+    view_port->model = NULL;
+    view_port->context = NULL;
     return view_port;
 }
 
@@ -48,7 +58,46 @@ void view_port_free(ViewPort* view_port) {
     furi_check(view_port->gui == NULL);
     furi_check(furi_mutex_release(view_port->mutex) == FuriStatusOk);
     furi_mutex_free(view_port->mutex);
+    view_port_free_model(view_port);
     free(view_port);
+}
+
+void view_port_allocate_model(ViewPort* view_port, ViewPortModelType type, size_t size) {
+    furi_check(view_port);
+    furi_check(size > 0);
+    furi_check(view_port->model_type == ViewPortModelTypeNone);
+    furi_check(view_port->model == NULL);
+    view_port->model_type = type;
+    if(view_port->model_type == ViewPortModelTypeLockFree) {
+        view_port->model = malloc(size);
+    } else if(view_port->model_type == ViewPortModelTypeLocking) {
+        ViewPortModelLocking* model = malloc(sizeof(ViewPortModelLocking) + size);
+        model->mutex = furi_mutex_alloc(FuriMutexTypeRecursive);
+        view_port->model = model;
+    } else {
+        furi_crash();
+    }
+}
+
+void view_port_free_model(ViewPort* view_port) {
+    furi_check(view_port);
+    if(view_port->model_type == ViewPortModelTypeNone) {
+        return;
+    } else if(view_port->model_type == ViewPortModelTypeLocking) {
+        ViewPortModelLocking* model = view_port->model;
+        furi_mutex_free(model->mutex);
+    }
+    free(view_port->model);
+    view_port->model = NULL;
+    view_port->model_type = ViewPortModelTypeNone;
+}
+
+static void view_unlock_model(ViewPort* view_port) {
+    furi_check(view_port);
+    if(view_port->model_type == ViewPortModelTypeLocking) {
+        ViewPortModelLocking* model = (ViewPortModelLocking*)(view_port->model);
+        furi_check(furi_mutex_release(model->mutex) == FuriStatusOk);
+    }
 }
 
 void view_port_set_input_callback(ViewPort* view_port, ViewPortInputCallback input_callback, void* input_context) {
@@ -67,19 +116,17 @@ void view_port_set_input_touch_callback(ViewPort* view_port, ViewPortInputTouchC
     furi_check(furi_mutex_release(view_port->mutex) == FuriStatusOk);
 }
 
-void view_port_set_layout_callback(ViewPort* view_port, ViewPortLayoutCallback layout_callback, void* layout_context) {
+void view_port_set_layout_callback(ViewPort* view_port, ViewPortLayoutCallback layout_callback) {
     furi_check(view_port);
     furi_check(furi_mutex_acquire(view_port->mutex, FuriWaitForever) == FuriStatusOk);
     view_port->layout.callback = layout_callback;
-    view_port->layout.context = layout_context;
     furi_check(furi_mutex_release(view_port->mutex) == FuriStatusOk);
 }
 
-void view_port_set_post_layout_callback(ViewPort* view_port, ViewPortLayoutCallback post_layout_callback, void* post_layout_context) {
+void view_port_set_post_layout_callback(ViewPort* view_port, ViewPortLayoutCallback post_layout_callback) {
     furi_check(view_port);
     furi_check(furi_mutex_acquire(view_port->mutex, FuriWaitForever) == FuriStatusOk);
     view_port->post_layout.callback = post_layout_callback;
-    view_port->post_layout.context = post_layout_context;
     furi_check(furi_mutex_release(view_port->mutex) == FuriStatusOk);
 }
 
@@ -132,7 +179,9 @@ void view_port_layout(ViewPort* view_port) {
     furi_check(view_port->gui);
 
     if(view_port->layout.callback) {
-        view_port->layout.callback(view_port->layout.context);
+        void* data = view_port_get_model(view_port);
+        view_port->layout.callback(data);
+        view_unlock_model(view_port);
     }
 
     furi_mutex_release(view_port->mutex);
@@ -143,14 +192,16 @@ void view_port_post_layout(ViewPort* view_port) {
 
     // We are not going to lockup system, but will notify you instead
     // Make sure that you don't call viewport methods inside of another mutex, especially one that is used in draw call
-    if(furi_mutex_acquire(view_port->mutex, 2) != FuriStatusOk) {
+    if(furi_mutex_acquire(view_port->mutex, 16) != FuriStatusOk) {
         FURI_LOG_W(TAG, "ViewPort lockup: see %s:%d", __FILE__, __LINE__ - 3);
     }
 
     furi_check(view_port->gui);
 
     if(view_port->post_layout.callback) {
-        view_port->post_layout.callback(view_port->post_layout.context);
+        void* data = view_port_get_model(view_port);
+        view_port->post_layout.callback(data);
+        view_unlock_model(view_port);
     }
 
     furi_mutex_release(view_port->mutex);
@@ -202,4 +253,22 @@ void view_port_update(ViewPort* view_port) {
 
     if(view_port->gui && view_port->is_enabled) gui_update(view_port->gui);
     furi_mutex_release(view_port->mutex);
+}
+
+void* view_port_get_model(ViewPort* view_port) {
+    furi_check(view_port);
+    if(view_port->model_type == ViewPortModelTypeLocking) {
+        ViewPortModelLocking* model = (ViewPortModelLocking*)(view_port->model);
+        furi_check(furi_mutex_acquire(model->mutex, FuriWaitForever) == FuriStatusOk);
+        return model->data;
+    }
+    return view_port->model;
+}
+
+void view_port_commit_model(ViewPort* view_port, bool update) {
+    furi_check(view_port);
+    view_unlock_model(view_port);
+    if(update) {
+        view_port_update(view_port);
+    }
 }
--- applications/services/gui/view_port.h
+++ applications/services/gui/view_port.h
@@ -10,8 +10,10 @@ typedef struct ViewPort ViewPort;
 
 /** ViewPort Layout callback
  * @warning    called from GUI thread
+ * @param      model   pointer to view model data
+ * @return true if view needs to be redrawn after layout, false otherwise
  */
-typedef void (*ViewPortLayoutCallback)(void* context);
+typedef bool (*ViewPortLayoutCallback)(void* model);
 
 /** ViewPort Input callback
  * @warning    called from GUI thread
@@ -25,17 +27,43 @@ typedef bool (*ViewPortInputCallback)(InputEvent* event, void* context);
  */
 typedef bool (*ViewPortInputTouchCallback)(InputTouchEvent* event, void* context);
 
+/** View model types */
+typedef enum {
+    /** Model is not allocated */
+    ViewPortModelTypeNone,
+    /** Model consist of atomic types and/or partial update is not critical for rendering.
+     * Lock free.
+     */
+    ViewPortModelTypeLockFree,
+    /** Model access is guarded with mutex.
+     * Locking gui thread.
+     */
+    ViewPortModelTypeLocking,
+} ViewPortModelType;
+
 ViewPort* view_port_alloc(void);
 
 void view_port_free(ViewPort* view_port);
+
+/** Allocate view model.
+ * @param view_port  ViewPort instance
+ * @param      type  View Model Type
+ * @param      size  size
+ */
+void view_port_allocate_model(ViewPort* view_port, ViewPortModelType type, size_t size);
+
+/** Free view model data memory.
+ * @param view_port  ViewPort instance
+ */
+void view_port_free_model(ViewPort* view_port);
 
 void view_port_set_input_callback(ViewPort* view_port, ViewPortInputCallback input_callback, void* input_context);
 
 void view_port_set_input_touch_callback(ViewPort* view_port, ViewPortInputTouchCallback input_touch_callback, void* input_touch_context);
 
-void view_port_set_layout_callback(ViewPort* view_port, ViewPortLayoutCallback layout_callback, void* layout_context);
+void view_port_set_layout_callback(ViewPort* view_port, ViewPortLayoutCallback layout_callback);
 
-void view_port_set_post_layout_callback(ViewPort* view_port, ViewPortLayoutCallback post_layout_callback, void* post_layout_context);
+void view_port_set_post_layout_callback(ViewPort* view_port, ViewPortLayoutCallback post_layout_callback);
 
 bool view_port_is_enabled(const ViewPort* view_port);
 
@@ -45,23 +73,49 @@ void view_port_set_enabled(ViewPort* view_port, bool enabled);
 
 void view_port_update(ViewPort* view_port);
 
-/**
- * @brief Glue function to put input events into a message queue, to use with view_port_set_input_callback
- * @warning Expects context to be of type FuriMessageQueue pointer
- * @param event 
- * @param context 
- * @return always true (the input is always consumed)
- */
-bool view_port_input_queue_glue(InputEvent* event, void* context);
+// /** Glue function to put input events into a message queue, to use with view_port_set_input_callback
+//  * @warning Expects context to be of type FuriMessageQueue pointer
+//  * @param event
+//  * @param context
+//  * @return always true (the input is always consumed)
+//  */
+// bool view_port_input_queue_glue(InputEvent* event, void* context);
 
-/**
- * @brief Glue function to put touch input events into a message queue to use with view_port_set_input_touch_callback
- * @warning Expects context to be of type FuriMessageQueue pointer
- * @param event 
- * @param context 
- * @return always true (the input is always consumed)
+// /** Glue function to put touch input events into a message queue to use with view_port_set_input_touch_callback
+//  * @warning Expects context to be of type FuriMessageQueue pointer
+//  * @param event
+//  * @param context
+//  * @return always true (the input is always consumed)
+//  */
+// bool view_port_input_touch_queue_glue(InputTouchEvent* event, void* context);
+
+/** Get view model data
+ * @param      view_port  ViewPort instance
+ * @return     pointer to model data
+ * @warning    Don't forget to commit model changes
  */
-bool view_port_input_touch_queue_glue(InputTouchEvent* event, void* context);
+void* view_port_get_model(ViewPort* view_port);
+
+/** Commit view model
+ * @param   view_port    ViewPort instance
+ * @param      update  true if you want to emit view update, false otherwise
+ */
+void view_port_commit_model(ViewPort* view_port, bool update);
+
+/** With clause for view model
+ *
+ * @param      view_port      ViewPort instance pointer
+ * @param      type           View model type
+ * @param      code           Code block that will be executed between model lock and unlock
+ * @param      update         Bool flag, if true, view will be updated after code block. Can be variable, so code block can decide if update is needed.
+ *
+ */
+#define with_view_model(view_port, type, code, update) \
+    {                                                  \
+        type = view_port_get_model(view_port);         \
+        {code};                                        \
+        view_port_commit_model(view_port, update);     \
+    }
 
 #ifdef __cplusplus
 }
