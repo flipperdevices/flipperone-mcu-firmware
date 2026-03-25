7cd0d1ae9a1dce3f1b5c572515eb7fedc9bf68dd
SG <who.just.the.doctor@gmail.com>
2026-03-25 04:37:32 +0000

    Logs: log level ant output settings

--- targets/f100/src/main.c
+++ targets/f100/src/main.c
@@ -36,7 +36,7 @@ int main(void) {
     furi_init();
 
     // TODO: read log level and output from nvm
-    furi_hal_log_init(FuriLogLevelInfo, FuriHalLogOutputAll);
+    furi_hal_log_init(FuriLogLevelInfo, FuriHalLogOutputRtt);
 
     // Critical FURI HAL
     furi_hal_init_early();
