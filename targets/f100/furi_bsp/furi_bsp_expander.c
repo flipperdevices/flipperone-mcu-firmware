d15befb47918fe5283c506484e3f360903f7b39f
SkorP <skorpionm@yandex.ru>
2026-03-26 20:29:15 +0400

    Revert "No reboot linux at reboot 2350"
    This reverts commit fc5f2ba4a16838f90f201fd4ca6c1f2b4c6292ab.

--- targets/f100/furi_bsp/furi_bsp_expander.c
+++ targets/f100/furi_bsp/furi_bsp_expander.c
@@ -4,8 +4,6 @@
 
 #include "furi_bsp_expander.h"
 
-#define TAG "BspExpander"
-
 typedef struct {
     Tca6416a* handle;
     FuriCallback callback;
@@ -32,6 +30,8 @@ typedef struct {
     ExpanderCallbackStorage expander7;
 } ExpanderMain;
 
+#define TAG "Expander"
+
 #define EXPANDER_MAIN_THREAD_FLAG_ISR 0x00000001
 
 // #define EXPANDER_DEBUG_ENABLE
@@ -54,10 +54,8 @@ static void furi_bsp_set_callback(ExpanderCallbackStorage* storage, FuriCallback
 
 static void furi_bsp_expander_control_init(void) {
     furi_check(expander_control == NULL);
-    FURI_LOG_I(TAG, "Initializing Control Expander");
     expander_control = malloc(sizeof(ExpanderControl));
     expander_control->handle = tca6416a_init(&furi_hal_i2c_handle_control, &gpio_expander_reset, &gpio_expander_int, TCA6416A_ADDRESS_A0);
-    tca6416a_write_output(expander_control->handle, 0x0000); // All outputs low by default
     tca6416a_write_mode(expander_control->handle, InputKeyMask);
 }
 
@@ -133,7 +131,6 @@ static int32_t furi_bsp_expander_callback_thread(void* context) {
 
 static void furi_bsp_expander_main_init(void) {
     furi_check(expander_main == NULL);
-    FURI_LOG_I(TAG, "Initializing Main Expander");
     expander_main = malloc(sizeof(ExpanderMain));
     expander_main->handle = tca6416a_init(&furi_hal_i2c_handle_main, &gpio_main_board_reset, &gpio_main_expander_int, TCA6416A_ADDRESS_A0);
 
@@ -145,8 +142,7 @@ static void furi_bsp_expander_main_init(void) {
     tca6416a_set_input_callback(expander_main->handle, furi_bsp_expander_main_interrupt_handler, expander_main);
     expander_main->control_state = FuriBspControlExpanderMainMcu;
     // Todo: Errata lay the I2C line
-    uint32_t output_mask = furi_bsp_expander_main_read_output();
-    furi_bsp_expander_main_write_output(output_mask | OutputExpMainVcc5v0DevS0En);
+    furi_bsp_expander_main_write_output(OutputExpMainVcc5v0DevS0En);
     tca6416a_write_mode(expander_main->handle, InputExpMainInputMask);
 
     expander_main->input_mask_old = ~tca6416a_read_input(expander_main->handle) & InputExpMainInputMask;
