eb4e199622b2a1945e122cbe468ef4cd154a41e6
SG <who.just.the.doctor@gmail.com>
2026-06-09 18:34:31 +0100

    CLI: simplify i2c command code

--- applications/services/cli/cli_command_i2c.c
+++ applications/services/cli/cli_command_i2c.c
@@ -16,7 +16,28 @@ typedef struct {
     const char* arg_spec;
     const char* description;
     bool (*execute)(PipeSide*, FuriString*);
-} I2cCmd;
+} I2CCmd;
+
+typedef struct {
+    const char* name;
+    const FuriHalI2cBusHandle* handle;
+} I2CBusInfo;
+
+const I2CBusInfo i2c_buses[] = {
+    {I2C_BUS_CONTROL, &furi_hal_i2c_handle_control},
+    {I2C_BUS_MAIN, &furi_hal_i2c_handle_main},
+};
+
+static const I2CBusInfo* cli_command_i2c_bus_by_name(FuriString* name) {
+    for(size_t i = 0; i < COUNT_OF(i2c_buses); i++) {
+        char name_buffer[16];
+        snprintf(name_buffer, sizeof(name_buffer), "%d", i);
+        if(furi_string_cmp(name, i2c_buses[i].name) == 0 || furi_string_cmp(name, name_buffer) == 0) {
+            return &i2c_buses[i];
+        }
+    }
+    return NULL;
+}
 
 static void cli_command_i2c_scan_bus(const FuriHalI2cBusHandle* handle, const char* bus_name) {
     furi_check(handle);
@@ -48,163 +69,162 @@ static bool cli_command_i2c_list(PipeSide* pipe, FuriString* args) {
     UNUSED(pipe);
     UNUSED(args);
     printf("Available I2C buses:\r\n");
-    printf(" 0 - %s\r\n", I2C_BUS_CONTROL);
-    printf(" 1 - %s\r\n", I2C_BUS_MAIN);
+    for(size_t i = 0; i < COUNT_OF(i2c_buses); i++) {
+        printf(" %d - %s\r\n", i, i2c_buses[i].name);
+    }
     return true;
 }
 
 static bool cli_command_i2c_search(PipeSide* pipe, FuriString* args) {
     UNUSED(pipe);
 
-    if(strcmp(furi_string_get_cstr(args), I2C_BUS_CONTROL) == 0 || strcmp(furi_string_get_cstr(args), "0") == 0) {
-        cli_command_i2c_scan_bus(&furi_hal_i2c_handle_control, I2C_BUS_CONTROL);
-    } else if(strcmp(furi_string_get_cstr(args), I2C_BUS_MAIN) == 0 || strcmp(furi_string_get_cstr(args), "1") == 0) {
-        cli_command_i2c_scan_bus(&furi_hal_i2c_handle_main, I2C_BUS_MAIN);
-    } else {
+    const I2CBusInfo* bus_info = cli_command_i2c_bus_by_name(args);
+    if(!bus_info) {
         printf(ANSI_FG_RED "Unknown I2C bus:" ANSI_RESET " %s\r\n", furi_string_get_cstr(args));
         return false;
     }
+
+    cli_command_i2c_scan_bus(bus_info->handle, bus_info->name);
     return true;
 }
 
 static bool cli_command_i2c_write(PipeSide* pipe, FuriString* args) {
     UNUSED(pipe);
-    const FuriHalI2cBusHandle* handle;
-    uint8_t device_address;
-    uint8_t device_register;
-    uint8_t* data;
-    size_t data_length;
-    FuriString* bus_name = furi_string_alloc();
+    bool result = false;
+    FuriString* arg_bus_name = furi_string_alloc();
+    FuriString* data_str = furi_string_alloc();
+    uint8_t* data = NULL;
 
-    if(furi_string_size(args)) {
-        args_read_string_and_trim(args, bus_name);
-        if(strcmp(furi_string_get_cstr(bus_name), I2C_BUS_CONTROL) == 0 || strcmp(furi_string_get_cstr(bus_name), "0") == 0) {
-            handle = &furi_hal_i2c_handle_control;
-        } else if(strcmp(furi_string_get_cstr(bus_name), I2C_BUS_MAIN) == 0 || strcmp(furi_string_get_cstr(bus_name), "1") == 0) {
-            handle = &furi_hal_i2c_handle_main;
-        } else {
-            printf(ANSI_FG_RED "Unknown I2C bus:" ANSI_RESET " %s\r\n", furi_string_get_cstr(bus_name));
-            furi_string_free(bus_name);
-            return false;
+    do {
+        uint8_t device_address;
+        uint8_t device_register;
+        size_t data_length;
+        const I2CBusInfo* bus_info;
+
+        if(!furi_string_size(args)) {
+            break;
         }
 
-        char* args_cstr = (char*)furi_string_get_cstr(args);
+        args_read_string_and_trim(args, arg_bus_name);
+        bus_info = cli_command_i2c_bus_by_name(arg_bus_name);
+
+        if(!bus_info) {
+            printf(ANSI_FG_RED "Unknown I2C bus:" ANSI_RESET " %s\r\n", furi_string_get_cstr(arg_bus_name));
+            break;
+        }
+
+        const char* args_cstr = furi_string_get_cstr(args);
         StrintParseError parse_err = StrintParseNoError;
         parse_err |= strint_to_uint8(args_cstr, &args_cstr, &device_address, 16);
         parse_err |= strint_to_uint8(args_cstr, &args_cstr, &device_register, 16);
         if(parse_err || (device_address > 0x7F) || ((device_address & 0x78) == 0) || ((device_address & 0x78) == 0x78)) {
-            furi_string_free(bus_name);
-            return false;
+            printf(ANSI_FG_RED "Invalid device address or register:" ANSI_RESET " %s\r\n", furi_string_get_cstr(args));
+            break;
         }
 
-        furi_string_printf(args, "%s", args_cstr);
-        furi_string_trim(args);
+        furi_string_set(data_str, args_cstr);
+        furi_string_trim(data_str);
 
-        data_length = furi_string_size(args) / 2;
+        data_length = furi_string_size(data_str) / 2;
         if(data_length == 0) {
             printf(ANSI_FG_RED "No data provided to write\r\n" ANSI_RESET);
-            furi_string_free(bus_name);
-            return false;
-        }        
+            break;
+        }
 
         data = malloc(data_length + 1); // +1 for device register
         data[0] = device_register;
-        if(!args_read_hex_bytes(args, data + 1, data_length)) {
+        if(!args_read_hex_bytes(data_str, data + 1, data_length)) {
             printf(ANSI_FG_RED "Failed" ANSI_RESET " to read hex data\r\n");
-            free(data);
-            furi_string_free(bus_name);
-            return false;
+            break;
         }
 
-    } else {
-        furi_string_free(bus_name);
-        return false;
-    }
+        furi_hal_i2c_acquire(bus_info->handle);
+        int success = furi_hal_i2c_master_tx_blocking(bus_info->handle, device_address, data, data_length + 1, FURI_HAL_I2C_TIMEOUT_US);
+        furi_hal_i2c_release(bus_info->handle);
 
-    furi_hal_i2c_acquire(handle);
-    int success = furi_hal_i2c_master_tx_blocking(handle, device_address, data, data_length + 1, FURI_HAL_I2C_TIMEOUT_US);
-    furi_hal_i2c_release(handle);
-
-    if(success >= PICO_OK) {
-        printf(ANSI_FG_GREEN "Written" ANSI_RESET " to device 0x%02X register 0x%02X:", device_address, device_register);
-        for(size_t i = 1; i < data_length + 1; i++) {
-            printf(" %02X", data[i]);
+        if(success >= PICO_OK) {
+            printf(ANSI_FG_GREEN "Written" ANSI_RESET " to device 0x%02X register 0x%02X:", device_address, device_register);
+            for(size_t i = 1; i < data_length + 1; i++) {
+                printf(" %02X", data[i]);
+            }
+            printf("\r\n");
+        } else {
+            printf(ANSI_FG_RED "Failed" ANSI_RESET " to write to bus %s device 0x%02X register 0x%02X\r\n", bus_info->name, device_address, device_register);
         }
-        printf("\r\n");
-    } else {
-        printf(
-            ANSI_FG_RED "Failed" ANSI_RESET " to write to bus %s device 0x%02X register 0x%02X\r\n",
-            furi_string_get_cstr(bus_name),
-            device_address,
-            device_register);
-    }
+
+        result = true;
+    } while(0);
 
     free(data);
-    furi_string_free(bus_name);
-    return true;
+    furi_string_free(arg_bus_name);
+    furi_string_free(data_str);
+    return result;
 }
 
 static bool cli_command_i2c_read(PipeSide* pipe, FuriString* args) {
     UNUSED(pipe);
 
-    const FuriHalI2cBusHandle* handle;
-    uint8_t device_address;
-    uint8_t device_register;
-    uint8_t length;
-    FuriString* bus_name = furi_string_alloc();
+    FuriString* arg_bus_name = furi_string_alloc();
+    bool result = false;
 
-    if(furi_string_size(args)) {
-        args_read_string_and_trim(args, bus_name);
-        if(strcmp(furi_string_get_cstr(bus_name), I2C_BUS_CONTROL) == 0 || strcmp(furi_string_get_cstr(bus_name), "0") == 0) {
-            handle = &furi_hal_i2c_handle_control;
-        } else if(strcmp(furi_string_get_cstr(bus_name), I2C_BUS_MAIN) == 0 || strcmp(furi_string_get_cstr(bus_name), "1") == 0) {
-            handle = &furi_hal_i2c_handle_main;
-        } else {
-            printf(ANSI_FG_RED "Unknown I2C bus:" ANSI_RESET " %s\r\n", furi_string_get_cstr(bus_name));
-            furi_string_free(bus_name);
-            return false;
+    do {
+        uint8_t device_address;
+        uint8_t device_register;
+        uint8_t length;
+        const I2CBusInfo* bus_info;
+
+        if(!furi_string_size(args)) {
+            break;
         }
 
-        char* args_cstr = (char*)furi_string_get_cstr(args);
+        args_read_string_and_trim(args, arg_bus_name);
+        bus_info = cli_command_i2c_bus_by_name(arg_bus_name);
+
+        if(!bus_info) {
+            printf(ANSI_FG_RED "Unknown I2C bus:" ANSI_RESET " %s\r\n", furi_string_get_cstr(arg_bus_name));
+            break;
+        }
+
+        const char* args_cstr = furi_string_get_cstr(args);
         StrintParseError parse_err = StrintParseNoError;
         parse_err |= strint_to_uint8(args_cstr, &args_cstr, &device_address, 16);
         parse_err |= strint_to_uint8(args_cstr, &args_cstr, &device_register, 16);
         parse_err |= strint_to_uint8(args_cstr, &args_cstr, &length, 10);
         if(parse_err || (device_address > 0x7F) || ((device_address & 0x78) == 0) || ((device_address & 0x78) == 0x78)) {
-            furi_string_free(bus_name);
-            return false;
+            printf(ANSI_FG_RED "Invalid device address or register:" ANSI_RESET " %s\r\n", furi_string_get_cstr(args));
+            break;
         }
-    } else {
-        furi_string_free(bus_name);
-        return false;
-    }
 
-    uint8_t* buffer = malloc(length);
+        {
+            uint8_t* buffer = malloc(length);
 
-    furi_hal_i2c_acquire(handle);
-    int success = furi_hal_i2c_master_trx_blocking(handle, device_address, &device_register, 1, buffer, length, FURI_HAL_I2C_TIMEOUT_US);
-    furi_hal_i2c_release(handle);
+            furi_hal_i2c_acquire(bus_info->handle);
+            int success = furi_hal_i2c_master_trx_blocking(bus_info->handle, device_address, &device_register, 1, buffer, length, FURI_HAL_I2C_TIMEOUT_US);
+            furi_hal_i2c_release(bus_info->handle);
 
-    if(success >= PICO_OK) {
-        printf(ANSI_FG_GREEN "Read" ANSI_RESET " from device 0x%02X register 0x%02X:", device_address, device_register);
-        for(size_t i = 0; i < length; i++) {
-            printf(" %02X", buffer[i]);
+            if(success >= PICO_OK) {
+                printf(ANSI_FG_GREEN "Read" ANSI_RESET " from device 0x%02X register 0x%02X:", device_address, device_register);
+                for(size_t i = 0; i < length; i++) {
+                    printf(" %02X", buffer[i]);
+                }
+                printf("\r\n");
+            } else {
+                printf(
+                    ANSI_FG_RED "Failed" ANSI_RESET " to read bus %s from device 0x%02X register 0x%02X\r\n", bus_info->name, device_address, device_register);
+            }
+
+            free(buffer);
         }
-        printf("\r\n");
-    } else {
-        printf(
-            ANSI_FG_RED "Failed" ANSI_RESET " to read bus %s from device 0x%02X register 0x%02X\r\n",
-            furi_string_get_cstr(bus_name),
-            device_address,
-            device_register);
-    }
 
-    free(buffer);
-    furi_string_free(bus_name);
-    return true;
+        result = true;
+    } while(0);
+
+    furi_string_free(arg_bus_name);
+
+    return result;
 }
 
-static const I2cCmd i2c_cmds[] = {
+static const I2CCmd i2c_cmds[] = {
     {"list", "", "List available I2C buses", cli_command_i2c_list},
     {"search", "<bus_name>", "Search for devices on the specified I2C bus", cli_command_i2c_search},
     {"write", "<bus_name> <device_hex> <reg_hex> <data_hex>", "Write data to the specified I2C device", cli_command_i2c_write},
@@ -214,7 +234,7 @@ static const I2cCmd i2c_cmds[] = {
 static void cli_command_i2c_print_usage(void) {
     printf("Usage:\r\ni2c <cmd>\r\nCmd list:\r\n");
     for(size_t i = 0; i < COUNT_OF(i2c_cmds); i++) {
-        const I2cCmd* c = &i2c_cmds[i];
+        const I2CCmd* c = &i2c_cmds[i];
         printf("\t%s %s - %s\r\n", c->name, c->arg_spec, c->description);
     }
 }
@@ -228,7 +248,7 @@ void cli_command_i2c(PipeSide* pipe, FuriString* args, void* context) {
         const char* cmd_str = furi_string_get_cstr(cmd);
         const char* arg_str = furi_string_get_cstr(args);
         for(size_t i = 0; i < COUNT_OF(i2c_cmds); i++) {
-            const I2cCmd* c = &i2c_cmds[i];
+            const I2CCmd* c = &i2c_cmds[i];
             if(strcmp(cmd_str, c->name) == 0) {
                 if(!c->execute(pipe, args)) {
                     printf("usage: i2c %s %s\r\n", c->name, c->arg_spec);
