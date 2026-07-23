a81f2e6d9f88542eb52d2a08776129cec1caff77
SkorP <skorpionm@yandex.ru>
2026-07-23 16:05:52 +0400

    refactoring

--- applications/services/rpc/rpc_input_handler.c
+++ applications/services/rpc/rpc_input_handler.c
@@ -10,24 +10,24 @@
  *                  LEFT=10, UP=11, PTT=12
  */
 static const InputKey proto_button_to_inputkey[] = {
-    [Flipper_One_Input_Button_OK]    = InputKeyOk,
-    [Flipper_One_Input_Button_BACK]  = InputKeyBack,
+    [Flipper_One_Input_Button_OK] = InputKeyOk,
+    [Flipper_One_Input_Button_BACK] = InputKeyBack,
     [Flipper_One_Input_Button_KEY_1] = InputKey1,
     [Flipper_One_Input_Button_KEY_2] = InputKey2,
     [Flipper_One_Input_Button_POWER] = InputKey3,
     [Flipper_One_Input_Button_KEY_4] = InputKey4,
     [Flipper_One_Input_Button_KEY_5] = InputKey5,
-    [Flipper_One_Input_Button_SW]    = InputKeySw,
-    [Flipper_One_Input_Button_DOWN]  = InputKeyDown,
+    [Flipper_One_Input_Button_SW] = InputKeySw,
+    [Flipper_One_Input_Button_DOWN] = InputKeyDown,
     [Flipper_One_Input_Button_RIGHT] = InputKeyRight,
-    [Flipper_One_Input_Button_LEFT]  = InputKeyLeft,
-    [Flipper_One_Input_Button_UP]    = InputKeyUp,
-    [Flipper_One_Input_Button_PTT]   = InputKeyPtt,
+    [Flipper_One_Input_Button_LEFT] = InputKeyLeft,
+    [Flipper_One_Input_Button_UP] = InputKeyUp,
+    [Flipper_One_Input_Button_PTT] = InputKeyPtt,
 };
 
 /* Map protobuf ButtonAction → system InputType */
 static const InputType proto_action_to_inputtype[] = {
-    [Flipper_One_Input_ButtonAction_PRESS]   = InputTypePress,
+    [Flipper_One_Input_ButtonAction_PRESS] = InputTypePress,
     [Flipper_One_Input_ButtonAction_RELEASE] = InputTypeRelease,
 };
 
@@ -52,10 +52,9 @@ void rpc_input_handler_callback(const Flipper_One_Rpc_RpcMessage* message, void*
         .type = proto_action_to_inputtype[evt->action],
         .sequence = 0,
     };
-
-    FURI_LOG_I(TAG, "Injecting %s %s",
-        input_get_key_name(event.key),
-        input_get_type_name(event.type));
+#ifdef SRV_RPC_DEBUG
+    FURI_LOG_I(TAG, "Injecting %s %s", input_get_key_name(event.key), input_get_type_name(event.type));
+#endif
 
     FuriPubSub* input_events = furi_record_open(RECORD_INPUT_EVENTS);
     furi_pubsub_publish(input_events, &event);
@@ -65,8 +64,8 @@ void rpc_input_handler_callback(const Flipper_One_Rpc_RpcMessage* message, void*
 /* Map protobuf TouchType → system InputTouchType */
 static const InputTouchType proto_touch_to_inputtype[] = {
     [Flipper_One_Input_TouchType_START] = InputTouchTypeStart,
-    [Flipper_One_Input_TouchType_MOVE]  = InputTouchTypeMove,
-    [Flipper_One_Input_TouchType_END]   = InputTouchTypeEnd,
+    [Flipper_One_Input_TouchType_MOVE] = InputTouchTypeMove,
+    [Flipper_One_Input_TouchType_END] = InputTouchTypeEnd,
 };
 
 void rpc_touch_handler_callback(const Flipper_One_Rpc_RpcMessage* message, void* context) {
@@ -87,10 +86,9 @@ void rpc_touch_handler_callback(const Flipper_One_Rpc_RpcMessage* message, void*
         .y = evt->y,
         .pressure = evt->pressure,
     };
-
-    FURI_LOG_I(TAG, "Injecting touch type=%d x=%ld y=%ld p=%ld",
-        event.type, event.x, event.y, event.pressure);
-
+#ifdef SRV_RPC_DEBUG
+    FURI_LOG_I(TAG, "Injecting touch type=%d x=%ld y=%ld p=%ld", event.type, event.x, event.y, event.pressure);
+#endif
     FuriPubSub* touch_events = furi_record_open(RECORD_INPUT_TOUCH_EVENTS);
     furi_pubsub_publish(touch_events, &event);
     furi_record_close(RECORD_INPUT_TOUCH_EVENTS);
